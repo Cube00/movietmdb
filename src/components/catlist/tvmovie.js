0e2a6a87e68d39056e01c179b007e576c76ae5a3
Your Name <coller.vato@gmail.com>
2021-05-14 13:06:39 +0400

    tvmovies fav func

--- src/components/catlist/tvmovie.js
+++ src/components/catlist/tvmovie.js
@@ -1,6 +1,13 @@
+import {useState, useEffect} from 'react';
 import {AiOutlineStar} from 'react-icons/ai';
+import {AiOutlineHeart} from 'react-icons/ai';
+import {AiFillHeart} from 'react-icons/ai';
+import {FiMoreVertical} from 'react-icons/fi';
+import { useFav } from "../useFav";
 
-const Tvmovie = ({slideContent}) => {
+const Tvmovie = ({slideContent, addFav}) => {
+  const [state, setState] = useState(false);
+  const [enable, setEnable] = useState(false);
   const post_path = 'https://image.tmdb.org/t/p/original';
   const {original_language, name, first_air_date,
     vote_average, vote_count, overview, poster_path, backdrop_path} = slideContent;
@@ -8,10 +15,40 @@ const Tvmovie = ({slideContent}) => {
   const cover = post_path + backdrop_path;
   const year = first_air_date.substring(0, 4);
 
+  useEffect(()=>{
+    const getItem = JSON.parse(localStorage.getItem('favourites') || 0);
+    if(getItem !== 0){
+      var filt = getItem.map((item)=> {return item.name === name});
+      if(filt.includes(true)){
+        setState(true);
+      }
+    }
+  },[state])
+
+  const handleClick = () =>{
+    setEnable(true);
+    if(enable===true){
+      setEnable(false)
+    }
+  }
+
   return <>
-    <div className="tvMovie">
+    <div className="tvMovie" onMouseLeave={()=>{setEnable(false)}}>
       <div className="cover-backdrop">
         <img src={cover} alt="" />
+      </div>
+      <div className="more_setts">
+        <FiMoreVertical onClick={()=>{handleClick()}} className="m_ic_sets"/>
+      </div>
+      <div className={enable === true ? "addit_blf active" : "addit_blf"}>
+        {state === true ? (
+          <button onClick={()=>{addFav(slideContent); setState(false)}} className="add_fav_tv">
+            <AiFillHeart/>
+          </button>) : (
+          <button onClick={()=>{addFav(slideContent); setState(true)}} className="add_fav_tv">
+            <AiOutlineHeart/>
+          </button>)
+        }
       </div>
       <h3>{year}</h3>
       <h2>{name}</h2>
--- src/components/catlist/tvshows.js
+++ src/components/catlist/tvshows.js
@@ -2,9 +2,11 @@ import {useState, useEffect} from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper.scss';
 import Tvmovie from './tvmovie';
+import { useFav } from "../useFav";
 
 const Tvshows = () => {
   const [movieDb,setMovieDb] = useState([]);
+  const { favourites, addFav } = useFav();
 
   useEffect(()=>{
     fetch('https://api.themoviedb.org/3/tv/popular?api_key=0c8eba5d41d59379c0d9a98afd4738fe&language=en-US&page=1')
@@ -18,10 +20,10 @@ const Tvshows = () => {
         <h2>Recently tv shows <span>Show More</span></h2>
       </div>
       {movieDb !== undefined ?
-        <Swiper spaceBetween={30} slidesPerView={3}>
+        <Swiper spaceBetween={30} navigation={true} slidesPerView={3}>
           {movieDb.slice(5,20).map((slideContent, index) => {
             return <SwiperSlide className="movie-item" key={slideContent.id}>
-              <Tvmovie slideContent={slideContent} />
+              <Tvmovie slideContent={slideContent} addFav={()=>addFav(slideContent)} />
             </SwiperSlide>
           })}
         </Swiper> : ''
